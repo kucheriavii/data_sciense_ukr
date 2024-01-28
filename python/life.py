ee42ed5fc3c7ac5db7fdfba24064f74007914132
r.ostapchuk <kucheriavii@gmail.com>
2024-01-28 16:13:08 +0200

    012 [Python] - Objects - Understanding Attributes and Methods_en.vtt

--- python/life.py
+++ python/life.py
@@ -1,1 +1,9 @@
-theAnswer = 42
+import math
+
+theAnswer = 45
+
+def qoute_marvin():
+    print("I've calculated your chance of survival, but I don't think you like it")
+
+def squere_root(x):
+    return math.sqrt(x)
